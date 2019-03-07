e3626d1bb4e482284b75d58f665654f21e6ee462
JohanHascoet <custum.johan@hotmail.fr>
2019-03-07 15:05:32 +0100

    Suppresion

--- app/src/main/java/com/example/applimedecin/MainActivity.java
+++ app/src/main/java/com/example/applimedecin/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private Integer t48155est;
-    private String uncoucou;
+
 }
